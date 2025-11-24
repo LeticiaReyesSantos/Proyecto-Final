021a4f5e3133a61e7b90e679457ade2a20c89445
julia <julia@Julian>
2025-11-24 14:28:40 -0400

    Actualizacion de la Clase enfermedad

--- Proyecto_Final/src/logico/Enfermedad.java
+++ Proyecto_Final/src/logico/Enfermedad.java
@@ -1,25 +1,34 @@
 package logico;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Enfermedad extends Diagnostico {
-	
-	/**
-	 * 
-	 */
+public class Enfermedad {
+	 
 	private static final long serialVersionUID = 1L;
+	private String codigo;
 	private String nombre;
+	private ArrayList<String> sintomas;
+	private String tratamiento;
 	private String tipo;
 	private boolean controlada;
 	
-	public Enfermedad(String codigo, LocalDateTime fecha, ArrayList<String> sintomas, String tratamiento, String nombre,
-			String tipo, boolean controlada) {
-		super(codigo, fecha, sintomas, tratamiento);
-		
+	public Enfermedad(String codigo, String nombre, String tratamiento, String tipo,
+			boolean controlada) {
+		super();
+		this.codigo = codigo;
 		this.nombre = nombre;
+		this.sintomas = new ArrayList<>();
+		this.tratamiento = tratamiento;
 		this.tipo = tipo;
-		this.controlada = false; //no esta controlada hasta que se indique lo contrario
+		this.controlada = controlada;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
 
 	public String getNombre() {
@@ -28,6 +37,22 @@ public class Enfermedad extends Diagnostico {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public ArrayList<String> getSintomas() {
+		return sintomas;
+	}
+
+	public void setSintomas(ArrayList<String> sintomas) {
+		this.sintomas = sintomas;
+	}
+
+	public String getTratamiento() {
+		return tratamiento;
+	}
+
+	public void setTratamiento(String tratamiento) {
+		this.tratamiento = tratamiento;
 	}
 
 	public String getTipo() {
@@ -45,5 +70,6 @@ public class Enfermedad extends Diagnostico {
 	public void setControlada(boolean controlada) {
 		this.controlada = controlada;
 	}
+	
 
 }
